1566043694b02dda21eaccb5b08b71921c1cfa11
vanmukilan <142682590+vanmukilan@users.noreply.github.com>
2023-09-05 19:33:40 +0530

    Add files via upload

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Mukilan</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I have 1year 4month Experience in web Developer.
+            <br /> and i currently study in full stack developer
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/picture3.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
